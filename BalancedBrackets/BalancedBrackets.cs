3994239f215256032396321cb79a5614f54171bf
Marcio Machado <marcio_mach@hotmail.com>
2019-04-15 23:10:29 -0300

    Criado Projeto BalancedBrackets

---
+++ BalancedBrackets/BalancedBrackets.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BalancedBrackets
+{
+    public static class BalancedBrackets
+    {
+        public static bool ExecuteValidBalancedBrackets(string expression)
+        {
+            string[] charactersAllowed = { "{", "}", "[", "]", "(", ")" };
+            var stack = new Stack<char>();
+            var symbols = expression.ToCharArray();
+            if (symbols.Length == 0)
+                throw new Exception("Please enter at least 1 characters");
+
+            foreach (var temp in symbols)
+            {
+                if (charactersAllowed.All(x => x != temp.ToString()))
+                    throw new Exception("Invalid characters");
+
+                if (temp == '[' || temp == '{' || temp == '(')
+                    stack.Push(temp);
+                else
+                {
+                    if (stack.Count == 0)
+                        return false;
+
+                    var top = stack.Peek();
+                    if (temp == ']')
+                    {
+                        if (top != '[')
+                            break;
+
+                        stack.Pop();
+                    }
+                    else if (temp == '}')
+                    {
+                        if (top != '{')
+                            break;
+
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        if (top != '(')
+                            break;
+
+                        stack.Pop();
+                    }
+                }
+            }
+            return !stack.Any();
+        }
+    }
+}
+
---
+++ BalancedBracketsTest/BalancedBracketsTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BalancedBracketsTest
+{
+    [TestClass]
+    public class BalancedBracketsTest
+    {
+        [TestMethod]
+        public void Should_Valid_BalancedBrackets_Examples1()
+        {
+            var valid = BalancedBrackets.BalancedBrackets.ExecuteValidBalancedBrackets("(){}[]");
+            Assert.IsTrue(valid);
+        }
+
+        [TestMethod]
+        public void Should_Valid_BalancedBrackets_Examples2()
+        {
+            var valid = BalancedBrackets.BalancedBrackets.ExecuteValidBalancedBrackets("[{()}](){}");
+            Assert.IsTrue(valid);
+        }
+
+        [TestMethod]
+        public void Should_Invalid_BalancedBrackets_Examples3()
+        {
+            var valid = BalancedBrackets.BalancedBrackets.ExecuteValidBalancedBrackets("[]{()");
+            Assert.IsFalse(valid);
+        }
+
+        [TestMethod]
+        public void Should_Invalid_BalancedBrackets_Examples4()
+        {
+            var valid = BalancedBrackets.BalancedBrackets.ExecuteValidBalancedBrackets("[{)]");
+            Assert.IsFalse(valid);
+        }
+
+        [TestMethod]
+        public void Should_Invalid_BalancedBrackets_Examples5()
+        {
+            var valid = BalancedBrackets.BalancedBrackets.ExecuteValidBalancedBrackets("{[}]");
+            Assert.IsFalse(valid);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Please enter at least 1 characters")]
+        public void Should_Return_Exception_BalancedBrackets_Examples6()
+        {
+            var valid = BalancedBrackets.BalancedBrackets.ExecuteValidBalancedBrackets("");
+            Assert.IsFalse(valid);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Invalid characters")]
+        public void Should_Return_Exception_BalancedBrackets_Examples7()
+        {
+            var valid = BalancedBrackets.BalancedBrackets.ExecuteValidBalancedBrackets("{a}[B]");
+            Assert.IsFalse(valid);
+        }
+    }
+}
